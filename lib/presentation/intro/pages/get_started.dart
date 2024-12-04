96182975a724e31884354505dec7b35c5f0a85d7
Farrel Muhammad <farrel.iqbal@creontek.com>
2024-12-04 21:09:56 +0700

    add new choose page theme

--- lib/presentation/intro/pages/get_started.dart
+++ lib/presentation/intro/pages/get_started.dart
@@ -4,6 +4,7 @@ import 'package:spotify_remake/common/widgets/button/basic_app_button.dart';
 import 'package:spotify_remake/core/configs/assets/app_images.dart';
 import 'package:spotify_remake/core/configs/assets/app_vectors.dart';
 import 'package:spotify_remake/core/configs/theme/app_colors.dart';
+import 'package:spotify_remake/presentation/choose_mode/pages/choose_mode.dart';
 
 class GetStartedPage extends StatelessWidget {
   const GetStartedPage({super.key});
@@ -14,56 +15,45 @@ class GetStartedPage extends StatelessWidget {
       body: Stack(
         children: [
           Container(
-            padding: EdgeInsets.symmetric(
-              vertical: 40,
-              horizontal: 40
-            ),
+            padding: EdgeInsets.symmetric(vertical: 40, horizontal: 40),
             decoration: const BoxDecoration(
-              image: DecorationImage(
-                fit: BoxFit.fill,
-                image: AssetImage(AppImages.introBG),
-              )
-          ),
-          child: Column(
-            children: [
-            Align(
-              alignment: Alignment.topCenter,
-              child: SvgPicture.asset(
-                AppVectors.logo,
-              ),
-            ),
-            Spacer(),
-            Text(
-              'Enjoy Listening To Music',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-                fontSize: 18
-              )
-            ),
-            SizedBox(height: 21),
-            Text(
-              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sagittis enim purus sed phasellus. Cursus ornare id scelerisque aliquam.',
-              style: TextStyle(
-                fontWeight: FontWeight.w500,
-                color: AppColors.gray,
-                fontSize: 13
-              ),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(height: 20),
-            BasicAppButton(
-              onPressed: () {
-                
-              }, 
-              title: 'Get Started',
-            )
-          ],
-          ),
+                image: DecorationImage(
+              fit: BoxFit.fill,
+              image: AssetImage(AppImages.introBG),
+            )),
           ),
           Container(
             color: Colors.black.withOpacity(0.15),
-          )
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 40),
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: SvgPicture.asset(
+                    AppVectors.logo,
+                  ),
+                ),
+                Spacer(),
+                Text('Enjoy Listening To Music',
+                    style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white, fontSize: 18)),
+                SizedBox(height: 21),
+                Text(
+                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sagittis enim purus sed phasellus. Cursus ornare id scelerisque aliquam.',
+                  style: TextStyle(fontWeight: FontWeight.w500, color: AppColors.grey, fontSize: 13),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 20),
+                BasicAppButton(
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => const ChooseModePage()));
+                  },
+                  title: 'Get Started',
+                )
+              ],
+            ),
+          ),
         ],
       ),
     );
